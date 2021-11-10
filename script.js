681f47641754d1330866c580c9195e8472d80e89
Alan <alanaa92@gmail.com>
2021-11-10 15:48:45 -0300

    Menu de selecionar contato e privacidade implementado

--- script.js
+++ script.js
@@ -5,16 +5,22 @@ let data;
 const conteudoChat = document.querySelector("section ul");
 let MenuLogin = document.querySelector(".tela-login")
 let userNameInput = document.querySelector(".tela-login input");
-let userLogin;
 let login;
+let userName = ""
+let userMsgPrivate = false;
+let userMsgTo = "Todos";
+const userSettings = document.querySelector(".encaminhado");
+
+userSettings.innerHTML = `Enviado para ${userMsgTo}`;
 
 function pedidoLogin() {
 
+    let userLogin;
     userLogin = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", { name: userNameInput.value });
     userLogin.then(esperarLogin);
+    userName = userNameInput.value;
 
 }
-
 
 setInterval(buscarDados, 3000);
 
@@ -27,11 +33,24 @@ function esperarLogin(resposta) {
     login = resposta.data
     if (login == "OK") {
         MenuLogin.classList.add("hidden")
+        ManterConexao();
     }
     else {
         alert("Usuario ja logado")
     }
 
+}
+
+function ManterConexao() {
+
+    setInterval(pedidoLoginContinuo, 5000)
+
+}
+
+function pedidoLoginContinuo() {
+
+    let userLogin;
+    userLogin = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", { name: userNameInput.value });
 }
 
 function esperarResposta(resposta) {
@@ -56,6 +75,14 @@ function BuscarMensagem() {
         let time = data[i].time;
         let type = data[i].type;
 
+        if (type == "private_message") {
+
+            if (to != userName || to != "Todos") {
+                continue;
+            }
+
+        }
+
         conteudoChat.innerHTML +=
             `<li class="caixa-mensagem ${type}" data-identifier="message">
                 <h1>${time} <strong> ${from}</strong> para <strong>${to}</strong>: ${text}</h1>
@@ -70,15 +97,58 @@ function BuscarMensagem() {
 }
 
 
+function escolherContato(item) {
+    let menuanterior = item.closest("ul");
+    let itemanterior = menuanterior.querySelector(".selecionado");
 
-function preencherChat() {
+    if (itemanterior === null) {
+        item.classList.add("selecionado");
+        check(item);
 
+    } else if (item === itemanterior) {
+        return;
 
+    } else {
+        itemanterior.classList.remove("selecionado");
+        check(itemanterior);
+        item.classList.add("selecionado");
+        check(item);
+        userMsgTo = item.querySelector("h1").innerHTML
+    }
 
-
+    userSettings.innerHTML = `Enviado para ${userMsgTo}`
 }
 
-//funcções para teste
+function escolherPrivacidade(item) {
+    let menuanterior = item.closest("ul");
+    let itemanterior = menuanterior.querySelector(".selecionado");
+    if (item === itemanterior) {
+        return;
+
+    } else {
+        itemanterior.classList.remove("selecionado");
+        check(itemanterior);
+        item.classList.add("selecionado");
+        check(item);
+        userMsgPrivate = !userMsgPrivate;
+    }
+
+    if (userMsgPrivate == true) {
+
+        userSettings.innerHTML = `Enviado para ${userMsgTo} (reservadamente)`;
+    }
+
+    else {
+        userSettings.innerHTML = `Enviado para ${userMsgTo}`;
+    }
+}
+
+function check(elemento) {
+    let checkativo;
+    checkativo = elemento.querySelector("ion-icon[name='checkmark']");
+    checkativo.classList.toggle("hidden");
+}
+
 
 function removerHidden(elemento) {
     elemento = elemento.closest("div")
